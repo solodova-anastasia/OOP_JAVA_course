7d80213fe7f1832951fc4d5d8f39ad9591bb7bf4
Anastasia_Sol <an.solodova29@gmail.com>
2023-07-29 13:54:41 +0700

    Cat_class_

--- Lesson_1/Cat.java
+++ Lesson_1/Cat.java
@@ -1,28 +1,36 @@
+package Lesson_1;
+
+import java.text.MessageFormat;
 public class Cat {
-    private String filename = null;
-    private int weight = 8;
-    private int age = 5;
-    private String name = null;
-    private String adress = null;
-    private String color = "Black";
+    private String name;
+    private  int age;
+    private Owner ownerName;
 
-    public void initialize(String name) {
-        this.filename = name;
+    public void setOwnerName(Owner ownerName) {
+        this.ownerName = ownerName;
     }
 
-    public void initialize(String name, int weight, int age) {
-        this.filename = name + weight + age;
+    public Owner getOwnerName() {
+        return this.ownerName;
     }
 
-    public void initialize(String name, int age) {
-        this.filename = name + age;
+    public void setName(String name){
+        this.name = name;
     }
 
-    public void initialize(int weight, String color) {
-        this.filename = weight + color;
+    public void setAge(int age){
+        this.age = age;
     }
 
-    public void initialize(int weight, String color, String adress) {
-        this.filename = weight + color + adress;
+    public String getName(){
+        return this.name;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public void greet(){
+        System.out.println(MessageFormat.format("Мяу! Меня зовут {0}. Мой возраст {1} года(лет). Мой владелец {2}.", name, age, ownerName.getName()));
     }
 }
